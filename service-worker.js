9d46e7f56a2917fdfde2e74d4e237ac1fe2ecd55
Abdón Rodríguez Davila <a@abdonrd.com>
2017-04-04 00:23:36 +0200

    Start the migration to Polymer 2.0

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2016 Abdón Rodríguez Davila (@abdonrd). All rights reserved.
+ * Copyright (c) 2017 Abdón Rodríguez Davila (@abdonrd). All rights reserved.
  * This code may only be used under the MIT style license found at https://abdonrd.github.io/LICENSE.txt
  */
 
--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,29 +1,16 @@
 /**
  * @license
- * Copyright (c) 2016 Abdón Rodríguez Davila (@abdonrd). All rights reserved.
+ * Copyright (c) 2017 Abdón Rodríguez Davila (@abdonrd). All rights reserved.
  * This code may only be used under the MIT style license found at https://abdonrd.github.io/LICENSE.txt
  */
 
+/* eslint-env node */
+
 module.exports = {
   staticFileGlobs: [
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-    '/data/**/*',
-    '/images/**/*',
     '/index.html',
-    '/manifest.json'
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/*'
   ],
-  navigateFallback: '/index.html',
-  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
-  runtimeCaching: [
-    {
-      urlPattern: /\/data\/.*json/,
-      handler: 'fastest',
-      options: {
-        cache: {
-          maxEntries: 10,
-          name: 'data-cache'
-        }
-      }
-    }
-  ]
+  navigateFallback: 'index.html'
 };
